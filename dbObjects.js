c3bdc537e9e04da903259a690563d8ec5bae5606
angi-mint <68991185+angi-mint@users.noreply.github.com>
2021-09-04 23:59:49 +0200

    added dbObjects.js

---
+++ dbObjects.js
@@ -0,0 +1,62 @@
+const Sequelize = require('sequelize');
+const { Collection } = require('discord.js');
+const sequelize = new Sequelize('database', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'sqlite',
+    logging: false,
+    storage: 'database.sqlite',
+});
+
+const Users = require('./models/Users.js')(sequelize, Sequelize.DataTypes);
+const CurrencyShop = require('./models/CurrencyShop.js')(sequelize, Sequelize.DataTypes);
+const UserItems = require('./models/UserItems.js')(sequelize, Sequelize.DataTypes);
+
+UserItems.belongsTo(CurrencyShop, { foreignKey: 'item_id', as: 'item' });
+
+/* eslint-disable-next-line func-names */
+Users.prototype.addItem = async function (item) {
+    const userItem = await UserItems.findOne({
+        where: { user_id: this.user_id, item_id: item.id },
+    });
+
+    if (userItem) {
+        userItem.amount += 1;
+        return userItem.save();
+    }
+
+    return UserItems.create({ user_id: this.user_id, item_id: item.id, amount: 1 });
+};
+
+/* eslint-disable-next-line func-names */
+Users.prototype.getItems = function () {
+    return UserItems.findAll({
+        where: { user_id: this.user_id },
+        include: ['item'],
+    });
+};
+
+const currency = new Collection();
+
+Reflect.defineProperty(currency, 'add', {
+    /* eslint-disable-next-line func-name-matching */
+    value: async function add(id, amount) {
+        const user = currency.get(id);
+        if (user) {
+            user.balance += Number(amount);
+            return user.save();
+        }
+        const newUser = await Users.create({ user_id: id, balance: amount });
+        currency.set(id, newUser);
+        return newUser;
+    },
+});
+
+Reflect.defineProperty(currency, 'getBalance', {
+    /* eslint-disable-next-line func-name-matching */
+    value: function getBalance(id) {
+        const user = currency.get(id);
+        return user ? user.balance : 0;
+    },
+});
+
+module.exports = { Users, CurrencyShop, UserItems, currency };
